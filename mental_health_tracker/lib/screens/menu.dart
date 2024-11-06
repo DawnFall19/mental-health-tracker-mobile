8aad20657f30a8b8467bfbb472ccf37623c370fb
Michael Ignasius <michael.ignasius@ui.ac.id>
2024-11-06 15:36:37 +0700

    Tutorial 7

--- mental_health_tracker/lib/screens/menu.dart
+++ mental_health_tracker/lib/screens/menu.dart
@@ -1,18 +1,16 @@
 import 'package:flutter/material.dart';
-
+import 'package:mental_health_tracker/widgets/left_drawer.dart';
+import 'package:mental_health_tracker/widgets/mood_card.dart'; 
 class MyHomePage extends StatelessWidget {
   final String npm = '2306259950';
   final String name = 'Michael Ignasius';
   final String className = 'PBP E';
-
   MyHomePage({super.key});
-
-  final List<ItemHomePage> items = [
-    ItemHomePage("Lihat Mood", Icons.mood),
-    ItemHomePage("Tambah Mood", Icons.add),
-    ItemHomePage("Logout", Icons.logout),
+  final List<ItemHomepage> items = [
+    ItemHomepage("Lihat Mood", Icons.mood),
+    ItemHomepage("Tambah Mood", Icons.add),
+    ItemHomepage("Logout", Icons.logout),
   ];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,7 +23,9 @@ class MyHomePage extends StatelessWidget {
           ),
         ),
         backgroundColor: Theme.of(context).colorScheme.primary,
+        iconTheme: const IconThemeData(color: Colors.white),
       ),
+      drawer: const LeftDrawer(),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
@@ -60,7 +60,7 @@ class MyHomePage extends StatelessWidget {
                     mainAxisSpacing: 10,
                     crossAxisCount: 3,
                     shrinkWrap: true,
-                    children: items.map((ItemHomePage item) {
+                    children: items.map((ItemHomepage item) {
                       return ItemCard(item);
                     }).toList(),
                   ),
@@ -73,13 +73,10 @@ class MyHomePage extends StatelessWidget {
     );
   }
 }
-
 class InfoCard extends StatelessWidget {
   final String title;
   final String content;
-
   const InfoCard({super.key, required this.title, required this.content});
-
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -96,58 +93,6 @@ class InfoCard extends StatelessWidget {
             const SizedBox(height: 8.0),
             Text(content),
           ],
-        ),
-      ),
-    );
-  }
-}
-
-class ItemHomePage {
-  final String name;
-  final IconData icon;
-
-  ItemHomePage(this.name, this.icon);
-}
-
-class ItemCard extends StatelessWidget {
-  final ItemHomePage item;
-
-  const ItemCard(this.item, {super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Material(
-      color: Theme.of(context).colorScheme.secondary,
-      borderRadius: BorderRadius.circular(12),
-
-      child: InkWell(
-        onTap: () {
-          ScaffoldMessenger.of(context)
-            ..hideCurrentSnackBar()
-            ..showSnackBar(
-              SnackBar(content: Text("Kamu telah menekan tombol ${item.name}!"))
-            );
-        },
-        child: Container(
-          padding: const EdgeInsets.all(8),
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(
-                  item.icon,
-                  color: Colors.white,
-                  size: 30.0,
-                ),
-                const Padding(padding: EdgeInsets.all(3)),
-                Text(
-                  item.name,
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(color: Colors.white),
-                ),
-              ],
-            ),
-          ),
         ),
       ),
     );
